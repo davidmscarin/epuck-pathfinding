a48e29afc286030ec0c18f6ac247b4032851aa3a
Gonto03 <gapmd2003@gmail.com>
2024-04-16 17:21:40 +0100

    basic example of usage of LIDAR's distance sensor to avoid collisions

---
+++ wall_following.py
@@ -0,0 +1,42 @@
+from controller import Robot, DistanceSensor
+from controllers.utils import cmd_vel
+
+# Create the Robot instance.
+robot: Robot = Robot()
+
+timestep: int = int(robot.getBasicTimeStep())  # in ms
+
+# TODO
+def getDistSensors():
+    sensors = []
+    for i in [0,1,6,7]:
+        ds: DistanceSensor = robot.getDevice("ps" + str(i))
+        ds.enable(timestep)
+        sensors.append(ds)
+    return sensors
+
+def obstacle_detected(dist_sensors):
+    THRESHOLD = 120
+    for ds in dist_sensors:
+        if ds.getValue() > THRESHOLD:
+            return True
+    return False
+
+def controller():
+    dist_sensors: [DistanceSensor] = getDistSensors()
+    i=0
+    while True:
+        readings = []
+        for ds in dist_sensors:
+            readings.append(ds.getValue())
+        print(readings)
+        cmd_vel(robot, 0.12, 0)
+        while not obstacle_detected(dist_sensors):
+            robot.step()
+        cmd_vel(robot, 0, 0.15)
+        while obstacle_detected(dist_sensors):
+            robot.step()
+
+
+if __name__ == "__main__":
+    controller()
